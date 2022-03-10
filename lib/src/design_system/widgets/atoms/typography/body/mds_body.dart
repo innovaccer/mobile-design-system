8a9fd9bc77febe81e1b1a07d941856a9df724a01
Akshat <akshat.kaneria@innovaccer.com>
2022-03-10 16:31:56 +0530

    added typography widgets and migrated to null safety

---
+++ lib/src/design_system/widgets/atoms/typography/body/mds_body.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:innovaccer_design_system/src/design_system/widgets/atoms/typography/scaler/text_scaler.dart';
+import 'package:innovaccer_design_system/src/theme/mixin/color_mixin.dart';
+import 'package:innovaccer_design_system/src/theme/mixin/font_mixin.dart';
+
+enum BodyAppearance {
+  /// cannot call it default as it is keyword
+  defaultType,
+  disabled,
+  error,
+  medium,
+  strong,
+  subtle,
+}
+
+class MDSBody extends StatelessWidget with ColorMixin, FontMixin {
+  final BodyAppearance? appearance;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? textOverflow;
+  final String text;
+  final Color? color;
+
+  MDSBody(
+    this.text, {
+    this.appearance = BodyAppearance.defaultType,
+    this.textAlign,
+    this.maxLines,
+    this.textOverflow,
+    this.color,
+  })  : assert(text.isNotEmpty);
+
+  @override
+  Widget build(BuildContext context) {
+    return _getTextWidget(
+      context,
+      textStyle: _getStyle(),
+    );
+  }
+
+  Widget copyWith(
+    BuildContext context, {
+    FontWeight? fontWeight,
+    double? fontSize,
+    List<Shadow>? shadows,
+    double? fontLineHeight,
+    double? letterSpacing,
+  }) {
+    // ignore: unnecessary_null_comparison
+    assert(context != null);
+    return _getTextWidget(
+      context,
+      fontLineHeight: fontLineHeight,
+      fontSize: fontSize,
+      textStyle: _getStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        shadows: shadows,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
+
+  Widget _getTextWidget(
+    BuildContext context, {
+    required TextStyle textStyle,
+    double? fontLineHeight,
+    double? fontSize,
+  }) {
+    double _textScaleFactor = TextScaler.of<TextScalingFactor>(context).scaleFactor;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: ((fontLineHeight ?? fontLineHeight24) -
+                  (fontSize ?? fontSize17)) /
+              2),
+      child: Text(
+        text,
+        style: textStyle,
+        textAlign: textAlign ?? TextAlign.left,
+        maxLines: maxLines,
+        overflow: textOverflow,
+        textScaleFactor: _textScaleFactor,
+      ),
+    );
+  }
+
+  TextStyle _getStyle({
+    double? fontSize,
+    FontWeight? fontWeight,
+    List<Shadow>? shadows,
+    double? letterSpacing,
+  }) {
+    return TextStyle(
+      fontSize: fontSize ?? fontSize17,
+      fontWeight: fontWeight ?? _fontWeightMapping[appearance],
+      color: color ?? _fontColorMapping[appearance],
+      letterSpacing: letterSpacing ?? letterSpacing1,
+      shadows: shadows,
+    );
+  }
+
+  Map<BodyAppearance, Color> get _fontColorMapping => {
+        BodyAppearance.defaultType: inverse,
+        BodyAppearance.disabled: inverseLightest,
+        BodyAppearance.error: alert,
+        BodyAppearance.medium: inverse,
+        BodyAppearance.strong: inverse,
+        BodyAppearance.subtle: inverseLighter
+      };
+
+  Map<BodyAppearance, FontWeight> get _fontWeightMapping => {
+        BodyAppearance.defaultType: FontWeight.w400,
+        BodyAppearance.disabled: FontWeight.w500,
+        BodyAppearance.error: FontWeight.w400,
+        BodyAppearance.medium: FontWeight.w500,
+        BodyAppearance.strong: FontWeight.w700,
+        BodyAppearance.subtle: FontWeight.w500
+      };
+}
---
+++ lib/src/design_system/widgets/atoms/typography/footnote/mds_footnote.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:innovaccer_design_system/src/design_system/widgets/atoms/typography/scaler/text_scaler.dart';
+import 'package:innovaccer_design_system/src/theme/mixin/color_mixin.dart';
+import 'package:innovaccer_design_system/src/theme/mixin/font_mixin.dart';
+
+enum FootnoteAppearance {
+  /// cannot call it default as it is keyword
+  defaultType,
+  disabled,
+  error,
+  medium,
+  strong,
+  subtle,
+}
+
+class MDSFootnote extends StatelessWidget with ColorMixin, FontMixin {
+  final FootnoteAppearance? appearance;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? textOverflow;
+  final String text;
+
+  MDSFootnote(
+    this.text, {
+    this.appearance,
+    this.textAlign,
+    this.maxLines,
+    this.textOverflow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (text.isEmpty) {
+      return Container();
+    }
+
+    double _textScaleFactor =
+        TextScaler.of<TextScalingFactor>(context).scaleFactor;
+
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(vertical: (fontLineHeight16 - fontSize13) / 2),
+      child: Text(
+        text,
+        style: _getStyle(),
+        textAlign: textAlign ?? TextAlign.left,
+        maxLines: maxLines,
+        overflow: textOverflow,
+        textScaleFactor: _textScaleFactor,
+      ),
+    );
+  }
+
+  TextStyle _getStyle() {
+    return TextStyle(
+      fontSize: fontSize13,
+      fontWeight: _fontWeightMapping[appearance],
+      color: _fontColorMapping[appearance],
+      letterSpacing: letterSpacing3,
+    );
+  }
+
+  Map<FootnoteAppearance, Color> get _fontColorMapping => {
+        FootnoteAppearance.defaultType: inverse,
+        FootnoteAppearance.disabled: inverseLightest,
+        FootnoteAppearance.error: alert,
+        FootnoteAppearance.medium: inverse,
+        FootnoteAppearance.strong: inverse,
+        FootnoteAppearance.subtle: inverseLighter
+      };
+
+  Map<FootnoteAppearance, FontWeight> get _fontWeightMapping => {
+        FootnoteAppearance.defaultType: FontWeight.w500,
+        FootnoteAppearance.disabled: FontWeight.w600,
+        FootnoteAppearance.error: FontWeight.w500,
+        FootnoteAppearance.medium: FontWeight.w600,
+        FootnoteAppearance.strong: FontWeight.w700,
+        FootnoteAppearance.subtle: FontWeight.w600
+      };
+}
