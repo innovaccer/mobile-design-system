fa1494af6bffe65071363f1c59793d096a9b0c69
Annsh Singh <annsh29@gmail.com>
2022-04-20 12:20:32 +0530

    Updated example app for interactions in badges

--- example/lib/screens/badge/mds_badge_screen.dart
+++ example/lib/screens/badge/mds_badge_screen.dart
@@ -9,7 +9,9 @@ class MDSBadgeScreen extends StatefulWidget {
 }
 
 class _MDSBadgeScreenState extends State<MDSBadgeScreen> with SpacingMixin, ColorMixin {
-  String defaultText = 'BADGE';
+  String _defaultText = 'BADGE';
+  BadgeType _badgeType = BadgeType.solid;
+  BadgeAppearance _badgeAppearance = BadgeAppearance.jal;
 
   @override
   Widget build(BuildContext context) {
@@ -20,61 +22,175 @@ class _MDSBadgeScreenState extends State<MDSBadgeScreen> with SpacingMixin, Colo
         ),
       ),
       body: Padding(
-        padding: p6,
+        padding: px6 + pt4,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            MDSBody(
-              'Solid',
-            ),
-            SizedBox(
-              height: spacing,
-            ),
-            Wrap(
-              direction: Axis.horizontal,
-              spacing: 16,
-              runSpacing: 16,
-              children: [
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.jal),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.stone),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.neem),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.haldi),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.mirch),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.tawak),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.nimbu),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.neel),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.solid, badgeAppearance: BadgeAppearance.jamun),
-              ],
-            ),
             SizedBox(
               height: spacing5,
             ),
-            MDSBody(
-              'Subtle',
-            ),
+            MDSBadge(badgeText: _defaultText, badgeType: _badgeType, badgeAppearance: _badgeAppearance),
             SizedBox(
-              height: spacing,
+              height: spacing5,
             ),
-            Wrap(
-              direction: Axis.horizontal,
-              spacing: 16,
-              runSpacing: 16,
-              children: [
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.jal),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.stone),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.neem),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.haldi),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.mirch),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.tawak),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.nimbu),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.neel),
-                MDSBadge(badgeText: defaultText, badgeType: BadgeType.subtle, badgeAppearance: BadgeAppearance.jamun),
-              ],
+            Divider(),
+            Expanded(
+              child: ListView(
+                padding: p0,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: MDSSubhead('Type'),
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: Wrap(
+                          children: [
+                            _typeOptionWidget(
+                              value: BadgeType.solid,
+                              groupValue: _badgeType,
+                              displayText: 'solid',
+                            ),
+                            _typeOptionWidget(
+                              value: BadgeType.subtle,
+                              groupValue: _badgeType,
+                              displayText: 'subtle',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: MDSSubhead('Appearance'),
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: Wrap(
+                          children: [
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.jal,
+                              groupValue: _badgeAppearance,
+                              displayText: 'jal',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.stone,
+                              groupValue: _badgeAppearance,
+                              displayText: 'stone',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.neem,
+                              groupValue: _badgeAppearance,
+                              displayText: 'neem',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.haldi,
+                              groupValue: _badgeAppearance,
+                              displayText: 'haldi',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.mirch,
+                              groupValue: _badgeAppearance,
+                              displayText: 'mirch',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.tawak,
+                              groupValue: _badgeAppearance,
+                              displayText: 'tawak',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.nimbu,
+                              groupValue: _badgeAppearance,
+                              displayText: 'nimbu',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.neel,
+                              groupValue: _badgeAppearance,
+                              displayText: 'neel',
+                            ),
+                            _appearanceOptionWidget(
+                              value: BadgeAppearance.jamun,
+                              groupValue: _badgeAppearance,
+                              displayText: 'jamun',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _typeOptionWidget({
+    required BadgeType value,
+    required BadgeType groupValue,
+    required String displayText,
+  }) {
+    return GestureDetector(
+      onTap: () => _updateType(value),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Radio(
+            visualDensity: VisualDensity.compact,
+            value: value,
+            groupValue: groupValue,
+            onChanged: _updateType,
+          ),
+          MDSBody(
+            displayText,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateType(BadgeType? value) {
+    setState(() {
+      _badgeType = value!;
+    });
+  }
+
+  Widget _appearanceOptionWidget({
+    required BadgeAppearance value,
+    required BadgeAppearance groupValue,
+    required String displayText,
+  }) {
+    return GestureDetector(
+      onTap: () => _updateAppearance(value),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Radio(
+            visualDensity: VisualDensity.compact,
+            value: value,
+            groupValue: groupValue,
+            onChanged: _updateAppearance,
+          ),
+          MDSBody(
+            displayText,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateAppearance(BadgeAppearance? value) {
+    setState(() {
+      _badgeAppearance = value!;
+    });
   }
 }
